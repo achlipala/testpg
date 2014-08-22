bd56bb892d24cff9cacebc90c434c6f94077f05f
Adam Chlipala <adam@chlipala.net>
2014-08-22 09:53:03 -0400

    Test Postgres env vars

--- server.js
+++ server.js
@@ -13,3 +13,15 @@ var port = process.env.OPENSHIFT_NODEJS_PORT;
 var ip = process.env.OPENSHIFT_NODEJS_IP;
 
 app.listen(port || 8080, ip);
+
+var connection_string = "postgres://localhost/openshift";
+
+if (process.env.OPENSHIFT_POSTGRES_DB_PASSWORD) {
+  connection_string = "postgres://"
+        + process.env.OPENSHIFT_POSTGRES_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_POSTGRES_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_POSTGRES_DB_HOST + ':' +
+        process.env.OPENSHIFT_POSTGRES_DB_PORT + '/openshift';
+}
+
+console.error("Postgres on " + connection_string);
